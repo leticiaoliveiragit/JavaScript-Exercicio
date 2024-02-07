a248bf8f035b55fc6d2c6064346261cd5f3a51f9
Leticia Oliveira <leh.andrades@hotmail.com>
2024-02-07 22:26:08 +0000

    Exercicio JavaScript

---
+++ Estrutura de decisão Javascript/estrutura.js
@@ -0,0 +1,21 @@
+var bebida ="3";
+
+switch(parseInt(bebida)){
+    case 1:
+        console.log("Vinho Verde");
+        break;
+    case 2:
+        console.log("Vinho do Porto");
+        break;
+    case 3:
+        console.log("Aqui está a sua bebida Ginja e o valor a ser pago é de €9.00");
+        break;
+    case 4:
+        console.log("Sangria")
+        break;
+
+    default:
+        console.log("Lamentamos, mas só temos: Café, Leite ou Chá.")
+ }
+
+ console.log("Posso lhe ajudar em mais alguma coisa?")
